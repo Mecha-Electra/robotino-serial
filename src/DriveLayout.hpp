5f56bc1cd7abf21d034ba9e4d719adfc54f07065
felipetrentin <felipetrentintecnico@hotmail.com>
2024-11-14 16:40:57 -0300

    better calibrated drive layout

--- src/DriveLayout.hpp
+++ src/DriveLayout.hpp
@@ -5,9 +5,9 @@ class DriveLayout
 {
 public:
 	DriveLayout()
-		: rb( 0.17f )
-		, rw( 0.06f )
-		, gear( 32.0f )
+		: rb( 0.13f )
+		, rw( 0.037f )
+		, gear( 16.0f )
 		, left_to_right_wheel( 0.52f )
 		, front_to_rear_wheel( 0.55f )
 		, encoder( 2000.0f )
